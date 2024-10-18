e41022283a03d3c1de87ce5a5464a120f970ddce
Michele Sonnessa <m.sonnessa@zero11.it>
2024-10-18 11:11:06 +0200

    example01- updated to Vaadin 24

---
+++ example01-helloworld/src/main/java/it/zero11/vaadin/course/Application.java
@@ -0,0 +1,26 @@
+package it.zero11.vaadin.course;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Hello World")
+@Theme("my-theme")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
